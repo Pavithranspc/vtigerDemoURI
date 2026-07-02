25df41084db9a3d5e498dd67e60e466da0378ae8
Ashish Priyadarshi <Ashish Priyadarshi@LAPTOP-TDMDK58Q.mshome.net>
2026-07-02 09:34:31 +0530

    Removed the Comments from TCOO3

--- Proj_vtigerCRM/src/test/java/org/vitger/campaign/TC003_verifyuserisabletocreatecampaignwithmandatoryfieldtest.java
+++ Proj_vtigerCRM/src/test/java/org/vitger/campaign/TC003_verifyuserisabletocreatecampaignwithmandatoryfieldtest.java
@@ -9,15 +9,7 @@ public class TC003_verifyuserisabletocreatecampaignwithmandatoryfieldtest {
 	@Test
 	public void test() {
 		
-		//launch chrome browser
-		//create object for ChromeDriver class
-//		ChromeDriver driver = new ChromeDriver();
-		
-		//create object for ChromeDriver class and store it in super interface.
-//		WebDriver driver = new ChromeDriver();
-		
-		//navigate to url
-//		driver.get("https://www.zomato.com/");
+
 		
 		Reporter.log("HELLO WORLD",true);		
 		
