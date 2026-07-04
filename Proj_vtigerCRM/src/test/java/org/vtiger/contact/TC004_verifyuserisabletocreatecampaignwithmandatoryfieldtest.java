860f61cd39830aa04e55113f8653299eec752102
Sharvanth <Sharvanth@LAPTOP-Q5EP9EKN>
2026-07-04 09:26:05 +0530

    TC004 added

---
+++ Proj_vtigerCRM/src/test/java/org/vtiger/contact/TC004_verifyuserisabletocreatecampaignwithmandatoryfieldtest.java
@@ -0,0 +1,27 @@
+package org.vtiger.contact;
+
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class TC004_verifyuserisabletocreatecampaignwithmandatoryfieldtest {
+
+	
+	@Test
+	public void test() {
+		
+		//launch chrome browser
+		//create object for ChromeDriver class
+//		ChromeDriver driver = new ChromeDriver();
+		
+		//create object for ChromeDriver class and store it in super interface.
+//		WebDriver driver = new ChromeDriver();
+		
+		//navigate to url
+//		driver.get("https://www.zomato.com/");
+		
+		Reporter.log("HELLO WORLD",true);		
+		
+	}
+	
+	
+}
